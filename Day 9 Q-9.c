bc8eae92fe0d4e1279959142899c77c0d4286943
Jatin Singh <Jatin.28117@stu.upes.ac.in>
2026-02-09 22:18:13 +0530

    Add program to reverse a string input

---
+++ Day 9 Q-9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    int i, len;
+
+    scanf("%s", s);          
+    len = strlen(s);
+
+    for (i = len - 1; i >= 0; i--) {
+        printf("%c", s[i]);
+    }
+
+    return 0;
+}
