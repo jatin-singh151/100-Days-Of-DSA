180a95ee11b58f841fe70c156c2b07c1f4019d0e
Jatin Singh <Jatin.28117@stu.upes.ac.in>
2026-02-14 11:35:05 +0530

    Create Day 14 Q14.c

---
+++ Day 14 Q14.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    int isIdentity = 1;
+
+    printf("Enter the order of matrix: ");
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    printf("Enter the elements of the matrix:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(i == j) {
+                if(matrix[i][j] != 1) {
+                    isIdentity = 0;
+                    break;
+                }
+            } else {
+                if(matrix[i][j] != 0) {
+                    isIdentity = 0;
+                    break;
+                }
+            }
+        }
+    }
+
+    if(isIdentity)
+        printf("Identity Matrix\n");
+    else
+        printf("Not an Identity Matrix\n");
+
+    return 0;
+}
