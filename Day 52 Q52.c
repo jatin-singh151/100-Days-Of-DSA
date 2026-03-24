ca9207a697cc4e2e9fc89f871552ba50d40b83cd
Jatin Singh <Jatin.28117@stu.upes.ac.in>
2026-03-24 22:23:40 +0530

    Create Day 52 Q52.c

---
+++ Day 52 Q52.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure of Tree Node
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+// Create new node
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Build tree from level order
+struct Node* buildTree(int arr[], int n, int i) {
+    if (i >= n || arr[i] == -1)
+        return NULL;
+
+    struct Node* root = newNode(arr[i]);
+
+    root->left = buildTree(arr, n, 2*i + 1);
+    root->right = buildTree(arr, n, 2*i + 2);
+
+    return root;
+}
+
+// LCA function
+struct Node* LCA(struct Node* root, int n1, int n2) {
+    if (root == NULL)
+        return NULL;
+
+    if (root->data == n1 || root->data == n2)
+        return root;
+
+    struct Node* left = LCA(root->left, n1, n2);
+    struct Node* right = LCA(root->right, n1, n2);
+
+    if (left && right)
+        return root;
+
+    return (left != NULL) ? left : right;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int x, y;
+    scanf("%d %d", &x, &y);
+
+    struct Node* root = buildTree(arr, n, 0);
+
+    struct Node* ans = LCA(root, x, y);
+
+    if (ans != NULL)
+        printf("%d\n", ans->data);
+
+    return 0;
+}
