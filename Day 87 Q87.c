5172f35cd0285fb3714426b82d040e29a997a175
Jatin Singh <Jatin.28117@stu.upes.ac.in>
2026-04-29 21:53:11 +0530

    Create Day 87 Q87.c

---
+++ Day 87 Q87.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+// Function to sort array (simple bubble sort)
+void sort(int arr[], int n) {
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Iterative Binary Search
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while(low <= high) {
+        int mid = (low + high) / 2;
+
+        if(arr[mid] == key)
+            return mid;
+        else if(arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    // Sort the array
+    sort(arr, n);
+
+    // Print sorted array
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
